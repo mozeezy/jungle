894f033f66ceab5b4a0f1b8013f092b22d0d5a83
Mohamed <justmoe23@gmail.com>
2022-02-08 19:34:57 -0500

    add rspec user tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       flash.notice = "You've entered the wrong email/password"
